2ee463737f56ad7fd5e3e302deb97a4238367053
ymaru <dyun022@gmail.com>
2022-08-12 17:11:44 +0900

    :art:Init: celebstars init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use("/static", express.static("static"));
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// localhost:8000/nextpage
+app.get("/nextpage", (req, res) => {
+  res.render("nextpage");
+});
+
+// localhost:8000/prepage
+app.get("/prepage", (req, res) => {
+  res.render("prepage");
+});
+
+app.listen(port, () => {
+  console.log("server open:", port);
+});
