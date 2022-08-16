91cc635b04fff05fe9bf4c7f3d4e9e9e738de479
ymaru <dyun022@gmail.com>
2022-08-16 18:47:47 +0900

    Feat: bootstrap, jquery add & profilepage add

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const app = express();
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
 
+// prepare server
+// redirect API calls
+// app.use("/api", api);
+// redirect root
+// app.use("/", express.static(__dirname + "/www"));
+// redirect bootstrap JS
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+// redirect JS jQuery
+app.use("/jq", express.static(__dirname + "/node_modules/jquery/dist"));
+// redirect CSS bootstrap
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+
 const port = 8000;
 
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("profile/profile");
 });
 
 // localhost:8000/nextpage
