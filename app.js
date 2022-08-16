b6515620db1dbef922a8708a99ed03293fa5501b
hi2102 <hi2102@naver.com>
2022-08-16 18:47:00 +0900

    jy: parallax scrolling page design 08.16.Tue

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ app.use("/static", express.static("static"));
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.render("index");
+	res.render("index");
 });
 
 // localhost:8000/nextpage
 app.get("/login", (req, res) => {
-  res.render("loginWeb");
+	res.render("loginWeb");
+});
+
+// localhost:8000/magazine
+app.get("/magazine", (req, res) => {
+	res.render("magazine");
 });
 
 app.listen(port, () => {
-  console.log("server open:", port);
+	console.log("server open:", port);
 });
