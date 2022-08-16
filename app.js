9d2f79aa43d9f35afe7c545681438de0d36c383e
hi2102 <hi2102@naver.com>
2022-08-16 14:23:07 +0900

    jy: test commit!

--- app.js
+++ app.js
@@ -7,21 +7,19 @@ app.use("/static", express.static("static"));
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.render("index");
+	res.render("index");
 });
 
 // localhost:8000/nextpage
-// localhost:8000/nextpage
-// localhost:8000/nextpage
 app.get("/nextpage", (req, res) => {
-  res.render("nextpage");
+	res.render("nextpage");
 });
 
 // localhost:8000/prepage
 app.get("/prepage", (req, res) => {
-  res.render("prepage");
+	res.render("prepage");
 });
 
 app.listen(port, () => {
-  console.log("server open:", port);
+	console.log("server open:", port);
 });
