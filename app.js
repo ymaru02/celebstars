17ed1b077cf1c29ee948b50714339021dd44bca5
Deng <edaero22@naver.com>
2022-08-18 14:21:41 +0900

    Feat: add bootstrap

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const { Router } = require("express");
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
+// 부트스트랩 적용
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 const port = 8000;
 
@@ -10,9 +14,9 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// localhost:8000/nextpage
-app.get("/login", (req, res) => {
-  res.render("loginWeb");
+// localhost:8000/logincorp
+app.get("/logincorp", (req, res) => {
+  res.render("login_corp");
 });
 
 app.listen(port, () => {
