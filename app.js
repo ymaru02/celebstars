1db01c4cc8996ae58179f020017b3e78eb46170d
ymaru <dyun022@gmail.com>
2022-08-19 16:39:26 +0900

    fix: magazine

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 app.use("/icon", express.static(__dirname + "/node_modules/bootstrap-icons"));
 // redirect slick-carousel CSS
 app.use(
-	"/slick",
-	express.static(__dirname + "/node_modules/slick-carousel/slick")
+  "/slick",
+  express.static(__dirname + "/node_modules/slick-carousel/slick")
 );
 
 const port = 8000;
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/profile", (req, res) => {
-	res.render("profile/profile");
+  res.render("profile/profile");
 });
 app.get("/profileupdate", (req, res) => {
   res.render("profile/profileUpdate");
@@ -45,7 +45,7 @@ app.get("/login", (req, res) => {
 
 // localhost:8000/magazine
 app.get("/magazine", (req, res) => {
-  res.render("magazine");
+  res.render("magazine/magazine");
 });
 
 app.listen(port, () => {
