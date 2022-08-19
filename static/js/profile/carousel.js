a7c59a3fabe83d6bb2cc5be392389f4c4a6aaf25
ymaru <dyun022@gmail.com>
2022-08-19 22:04:51 +0900

    Feat: img lightbox add

--- static/js/profile/carousel.js
+++ static/js/profile/carousel.js
@@ -7,15 +7,9 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     speed: 100, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
     prevArrow:
-      "<button type='button' class='slick-prev slick-button'>Previous</button>", // 이전 화살표 모양 설정
+      "<button type='button' class='slick-prev slick-button'>&#10094;</button>", // 이전 화살표 모양 설정
     nextArrow:
       "<button type='button' class='slick-next slick-button'>Next</button>", // 다음 화살표 모양 설정
     variableWidth: true,
   });
 });
-
-function hiddenScroll() {
-  document.body.className = "";
-}
-
-// $(".center img").on("click", function () {});
---
+++ static/js/profile/lightBox.js
@@ -0,0 +1,41 @@
+function hiddenScroll() {
+  document.body.className = "";
+}
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
