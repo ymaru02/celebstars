f2157ce7203f5ff045ba48426bf702ee7d84350e
Deng <edaero22@naver.com>
2022-08-19 18:11:25 +0900

    Feat: add animation

--- static/js/back_to_top/back_to_top.js
+++ static/js/back_to_top/back_to_top.js
@@ -2,6 +2,7 @@ $(".back_to_top").on("click", function (e) {
   e.preventDefault();
   $("html,body").animate({ scrollTop: 0 }, 200);
 });
+
 $(window).on("scroll", function () {
   if ($(document).scrollTop() > 100) {
     $(".back_to_top").addClass("show");
