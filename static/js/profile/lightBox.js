a6ece74001ed373bfa4371522c00723a228e29d4
ymaru <dyun022@gmail.com>
2022-08-23 03:40:12 +0900

    Fix: profile Page color change

--- static/js/profile/lightBox.js
+++ static/js/profile/lightBox.js
@@ -3,39 +3,83 @@ function hiddenScroll() {
 }
 
 // Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openModal(target) {
+  if (target === "img") {
+    document.getElementById("img-modal").style.display = "block";
+  } else {
+    document.getElementById("video-modal").style.display = "block";
+  }
+  document.body.classList.add("light-box");
 }
 
 // Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModal(target) {
+  if (target === "img") {
+    document.getElementById("img-modal").style.display = "none";
+  } else {
+    document.getElementById("video-modal").style.display = "none";
+  }
+  document.body.classList.remove("light-box");
 }
 
-function showSlides(n) {
+function showImgSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("myImgSlides");
   if (n > slides.length) {
-    slideIndex = 1;
+    slideImgIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideImgIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideImgIndex - 1].style.display = "block";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideImgIndex = 1;
+showImgSlides(slideImgIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function plusImgSlides(n) {
+  showImgSlides((slideImgIndex += n));
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function currentImgSlide(n) {
+  showImgSlides((slideImgIndex = n));
 }
+
+function showVideoSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("myVideoSlides");
+  if (n > slides.length) {
+    slideVideoIndex = 1;
+  }
+  if (n < 1) {
+    slideVideoIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideVideoIndex - 1].style.display = "block";
+}
+
+var slideVideoIndex = 1;
+showVideoSlides(slideVideoIndex);
+
+// Next/previous controls
+function plusVideoSlides(n) {
+  showVideoSlides((slideVideoIndex += n));
+}
+
+// Thumbnail image controls
+function currentVideoSlide(n) {
+  showVideoSlides((slideVideoIndex = n));
+}
+
+$(document).ready(function () {
+  $(".inner img").hover(function () {
+    $(this).css("cursor", "pointer");
+  });
+});
