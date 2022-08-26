f85d03da5138888ceff73ff1795bb9b2a25b8bac
hi2102 <hi2102@naver.com>
2022-08-26 16:33:11 +0900

    Feat: add a func to dynamically change the title

--- static/js/magazine_scroll/magazine_scroll.js
+++ static/js/magazine_scroll/magazine_scroll.js
@@ -3,9 +3,9 @@
 let imgs = document.querySelectorAll(".magazine_bg_img");
 // 버튼 눌러 다음 페이지로 이동
 const downBtn = document.querySelector(".button_down");
-downBtn.addEventListener("click", lazyLoad);
+downBtn.addEventListener("click", scrollDetection);
 
-function lazyLoad() {
+function scrollDetection() {
 	if (window.scrollY === 0) {
 		window.scrollTo({ top: imgs[1].offsetTop, behavior: "smooth" });
 	} else {
@@ -16,16 +16,13 @@ function lazyLoad() {
 		}
 	}
 }
+
 // 스크롤 하단 감지
 window.addEventListener("scroll", function () {
 	const scrollY = window.scrollY;
 	const offsetHeight = document.body.offsetHeight * 12;
 	const IS_END = scrollY >= offsetHeight - 150;
 	const HIDDEN_CLASS = "magazine_hidden";
-
-	// const windowHeight = window.innerHeight;
-	// const IS_BOTTOM = windowHeight + scrollY === offsetHeight;
-	// console.log( scrollY, offsetHeight)
 
 	if (IS_END) {
 		downBtn.classList.add(HIDDEN_CLASS);
@@ -37,3 +34,10 @@ window.addEventListener("scroll", function () {
 $(".to_the_top").on("click", () => {
 	window.scrollTo({ top: imgs[0].offsetTop, behavior: "smooth" });
 });
+
+// 아티스트 이름에 따라서 동적으로 타이틀 변경
+const changeTitle = () => {
+	let artistName = document.querySelector(".artist_name");
+	document.title = artistName.innerText;
+};
+changeTitle();
