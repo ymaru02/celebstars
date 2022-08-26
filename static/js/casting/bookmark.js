3737b762ec051765760e9bce7268f1a1e3e1ad3c
ymaru <dyun022@gmail.com>
2022-08-26 17:04:58 +0900

    Feat: open page add

--- static/js/casting/bookmark.js
+++ static/js/casting/bookmark.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  $(".card-bookmark").on("click", function () {
+  $(".card-bookmark").on("click", function (event) {
+    event.preventDefault();
     if ($(this).hasClass("bi-bookmark")) {
       $(this).addClass("bi-bookmark-star-fill");
       $(this).removeClass("bi-bookmark");
